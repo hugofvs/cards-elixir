403a945a2abedd3aff33f38f8a1fc3aa06b7a855
Hugo Silva <hugo.f.v.silva@gmail.com>
2018-01-12 16:19:14 +0000

    Add documentations to Cards methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,8 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
   def create_deck do
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -12,11 +16,29 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
-  # question mark is used when returning true or false
+  @doc """
+    Determines wether a deck contains a given card
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, hand) do
     Enum.member?(deck, hand)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
